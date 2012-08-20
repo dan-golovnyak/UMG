5a4e4f989cce0ab46638c3f07048517f84ac9863
Matt Korostoff <MKorostoff@gmail.com>
2012-08-20 10:52:20 -0400

    Updating styles

--- js/misc.js
+++ js/misc.js
@@ -1,4 +1,4 @@
-//Change the text of the Previous/Next buttons
+//Change the text of the Previous/Next buttons.
 (function($) {
 	$('li.pager-next a').html('Next »');
 	$('li.pager-previous a').html('« Prev');
