d2aeb1c4f219f102253dc0aaabaa978973f01d25
Matt Korostoff <MKorostoff@gmail.com>
2012-08-24 19:56:32 -0400

    updating replacements

--- replace.php
+++ replace.php
@@ -6,20 +6,23 @@ $css_file = file_get_contents('umg-styles.css');
 
 //we want to search for these strings and replace them with the strings bellow
 $original_strings = array(
-	"/\.container/",
-	"/\.span-8/",
-	"/\.navigation/",
-	"/\#bd/",
-	"/\#top-nav/",
-	"/\#ft /",
-	"/\.bg_nav/",
-	"/h1\#logo/",
-	"/\.pg-home/",
-	"/\#hd/",
-	"/li.selected a",
-	"/li.selected",
-	"/\.primary "
-
+	"/\.container/"
+	"/\.span-8/"
+	"/\.navigation/"
+	"/\#bd/"
+	"/\#top-nav/"
+	"/\#ft /"
+	"/\#ft,/"
+	"/\.bg_nav/"
+	"/h1\#logo/"
+	"/\.pg-home/"
+	"/\#hd/"
+	"/li\.selected a/"
+	"/li\.selected/"
+	"/\.primary /"
+	"/\.primary,/"
+	"/h2\.s_channel_title/"
+	"/\.secondary h1 a/"
 );
 
 //We want to use these strings to replace the stings above
@@ -29,15 +32,18 @@ $replacement_strings = array(
 	".menu",
 	"#content",
 	"#navigation",
-	"#copyright",
+	"#copyright ",
+	"#copyright,",
 	"#header",
 	"#site-name",
 	".front",
 	"#banner",
 	"li a.active",
 	"li a.active",
-	"#main "
-
+	"#main ",
+	"#main,",
+	".middle > h2, .view-content > h3, h1#page-title",
+	"#site-name a"
 );
 
 $new_css = preg_replace($original_strings, $replacement_strings, &$css_file);
