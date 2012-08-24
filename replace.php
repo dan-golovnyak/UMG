24618ec1d343d1f7da7dfaae1d0a946ccd7d2507
Matt Korostoff <MKorostoff@gmail.com>
2012-08-24 18:32:42 -0400

    find replace script

---
+++ replace.php
@@ -0,0 +1,37 @@
+<?php
+error_reporting(0);
+
+//First we pull in the local css file
+$css_file = file_get_contents('umg-styles.css');
+
+//we want to search for these strings and replace them with the strings bellow
+$original_strings = array(
+	"/\.container/",
+	"/\.span-8/",
+	"/\.navigation/",
+	"/\#bd/",
+	"/\#top-nav/",
+	"/\#ft /",
+	"/\.bg_nav/",
+	"/h1\#logo/",
+	"/\.pg-home/",
+	"/\#hd/"
+);
+
+//We want to use these strings to replace the stings above
+$replacement_strings = array(
+	".page-width",
+	".sidebar-a",
+	".menu",
+	"#content",
+	"#navigation",
+	"#copyright",
+	"#header",
+	"#site-name",
+	".front",
+	"#banner"
+);
+
+$new_css = preg_replace($original_strings, $replacement_strings, &$css_file);
+
+print $new_css;
